02343316d8574f7e857b13cd7cebf25867d10d4a
Evan McKnight <emcknight@captechventures.com>
2022-10-27 17:45:46 -0400

    idk where this pushes to

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , redirectTo: 'home', pathMatch: 'full'},
+{path: 'home' , component: HomeComponent},
+{path: 'resume' , component: ResumeComponent},
+{path: 'about' , component: AboutComponent},
+{path: 'projects' , component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
+}) 
 export class AppRoutingModule { }
+ 
